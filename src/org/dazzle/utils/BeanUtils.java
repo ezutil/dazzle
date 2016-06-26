4bc091bce0e62ae05ae935c35d9d289649fb351e
hcqt <hcqt@qq.com>
2016-06-26 15:02:34 +0800

    增加注释与测试类

--- src/org/dazzle/utils/BeanUtils.java
+++ src/org/dazzle/utils/BeanUtils.java
@@ -1,70 +1,95 @@
 package org.dazzle.utils;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.dazzle.common.exception.BaseException;
 
-/** @author hcqt@qq.com */
+/**JavaBean与Map的相互转化，JavaBean与JavaBean的相互转化<br />
+ * 兼容父类的字段；底层实现按照Bean内的字段名称进行转换，而非传统的java数据类型继承
+ * @author hcqt@qq.com */
 public class BeanUtils {
 
 	/** @author hcqt@qq.com */
 	BeanUtils() { super(); }
 
-	/** @author hcqt@qq.com */
-	public static final <T> Map<String, Object> bean2Map(
-			T bean) {
-		return bean2Map(bean, (String[]) null, false);
+	/**@see #bean2Map(Object, String[], Boolean)
+	 * @author hcqt@qq.com */
+	public static final <T> Map<String, Object> bean2Map(T bean) {
+		return bean2Map(bean, (Object) null, false);
 	}
 
-	/** @author hcqt@qq.com */
+	/**@see #bean2Map(Object, String[], Boolean)
+	 * @author hcqt@qq.com */
 	public static final <T> Map<String, Object> bean2Map(
 			T bean, 
 			String[] fields) {
 		return bean2Map(bean, fields, false);
 	}
 	
-	/** @author hcqt@qq.com */
+	/**@see #bean2Map(Object, String[], Boolean)
+	 * @author hcqt@qq.com */
 	public static final <T> Map<String, Object> bean2Map(
 			T bean, 
 			String fields) {
-		return bean2Map(bean, fields, false);
+		return bean2Map(bean, (Object) fields, false);
 	}
 
-	/** @author hcqt@qq.com */
+	/**@see #bean2Map(Object, String[], Boolean)
+	 * @author hcqt@qq.com */
 	public static final <T> Map<String, Object> bean2Map(
 			T bean, 
 			Boolean needSequence) {
 		return bean2Map(bean, (String[]) null, needSequence);
 	}
 
-	/** @author hcqt@qq.com */
+	/**@see #bean2Map(Object, String[], Boolean)
+	 * @author hcqt@qq.com */
 	public static final <T> Map<String, Object> bean2Map(
 			T bean, 
 			String fields, 
 			Boolean needSequence) {
-		String[] fieldsArray = null;
-		if(null != fields) {
-			fieldsArray = fields.split(",");
-			for (int i = 0; i < fieldsArray.length; i++) {
-				if(null == fieldsArray[i] || fieldsArray[i].trim().isEmpty()) {
-					continue;
-				}
-				fieldsArray[i] = fieldsArray[i].trim();
-			}
-		}
-		return bean2Map(bean, fieldsArray, needSequence);
+		return bean2Map(bean, (Object) fields, needSequence);
 	}
 
-	/** @author hcqt@qq.com */
+	/**把bean的指定字段放入map
+	 * @param fields 指定参与转化你的字段，允许采用逗号分隔的字符串/字符串数组/字符串Collection
+	 * @see #bean2Map(Object, String[], Boolean)
+	 * @author hcqt@qq.com
+	 */
+	public static final <T> Map<String, Object> bean2Map(
+			T bean, 
+			Object fields, 
+			Boolean needSequence) {
+		if(fields == null) {
+			return bean2Map(bean, (String[]) null, needSequence);
+		}
+		String[] _fields = null;
+		if(fields instanceof String) {
+			_fields = DTU.cvt(String[].class, ((String) fields).split(","));
+		} else {
+			try {
+				_fields = DTU.cvt(String[].class, fields);
+			} catch (BaseException e) {
+				throw new BaseException("beanUtils_9k3YP", "转换bean到map，指定字段的书写存在错误，只允许采用逗号分隔的字符串/字符串数组/字符串Collection，您用类型:{0}数据:{1}指定字段是不符合规则的", e, fields.getClass(), fields);
+			}
+		}
+		return bean2Map(bean, _fields, needSequence);
+	}
+
+	/**把bean的指定字段放入map
+	 * @param bean 数据源bean
+	 * @param fields 指定要转换bean的哪些字段
+	 * @param needSequence 返回的map是否需要保持指定的顺序
+	 * @author hcqt@qq.com */
+	@SuppressWarnings("unchecked")
 	public static final <T> Map<String, Object> bean2Map(
 			T bean, 
 			String[] fields, 
@@ -72,23 +97,23 @@ public class BeanUtils {
 		if (null == bean) {
 			return null;
 		}
-		Map<String, Object> ret = null;
-		if(null == needSequence) {
-			needSequence = false;
-		}
-		if(needSequence) {
-			ret = new LinkedHashMap<String, Object>();
-		} else {
-			ret = new HashMap<String, Object>();
-		}
 		Class<?> clazz = bean.getClass();
-		List<String> fieldList = null;
+		Set<String> fieldSet = null;
 		if(null == fields) {
-			fieldList = getAllFieldName(clazz);
+			fieldSet = getAllFieldName(clazz);
 		} else {
-			fieldList = Arrays.asList(fields);
+			fieldSet = DTU.cvt(Set.class, fields);
 		}
-		for (String field : fieldList) {
+		if(null == fieldSet) {
+			return null;
+		}
+		Map<String, Object> ret = null;
+		if(needSequence == null || !needSequence) {
+			ret = new HashMap<String, Object>();
+		} else {
+			ret = new LinkedHashMap<String, Object>();
+		}
+		for (String field : fieldSet) {
 			if(null == field) {
 				continue;
 			}
@@ -97,9 +122,14 @@ public class BeanUtils {
 		return ret;
 	}
 
-	/** @author hcqt@qq.com */
+	/**把一个bean的字段逐个放入另一个bean的同名字段内
+	 * @see #bean2Bean(Map, Object, Object)
+	 * @author hcqt@qq.com */
 	public static final <S, T> T bean2Bean(S obj, Class<T> clazz) {
-		List<String> fields = getAllFieldName(clazz);
+		Set<String> fields = getAllFieldName(clazz);
+		if(fields == null) {
+			return null;
+		}
 		Map<String, String> fieldMapping = new HashMap<String, String>(); 
 		for (String field : fields) {
 			fieldMapping.put(field, field);
@@ -107,34 +137,125 @@ public class BeanUtils {
 		return bean2Bean(fieldMapping, obj, clazz);
 	}
 
-	/** @author hcqt@qq.com */
+	/**@see #bean2Bean(Map, Object, Object)
+	 * @author hcqt@qq.com */
+	public static final <S, T> T bean2Bean(S obj, T target) {
+		if(target == null) {
+			return null;
+		}
+		Set<String> fields = getAllFieldName(target.getClass());
+		if(fields == null) {
+			return target;
+		}
+		Map<String, String> fieldMapping = new HashMap<String, String>(); 
+		for (String field : fields) {
+			fieldMapping.put(field, field);
+		}
+		return bean2Bean(fieldMapping, obj, target);
+	}
+
+	/**@param <S> 源数据的Javabean类型
+	 * @param <T> 目标数据类型
+	 * @param fieldMapping 源数据bean字段与目标bean字段的映射关系
+	 * @param obj 源数据bean
+	 * @param clazz 目标数据类型
+	 * @see #bean2Bean(Map, Object, Object)
+	 * @author hcqt@qq.com */
 	public static final <S, T> T bean2Bean(Map<String, String> fieldMapping, S obj, Class<T> clazz) {
 		if(null == fieldMapping) {
 			return null;
 		}
+		T ret = null;
 		try {
-			T ret = clazz.newInstance();
-			for (Entry<String, String> entry : fieldMapping.entrySet()) {
-				Object fieldNewValue = getFieldValue(obj, entry.getKey());
-				setFieldValue(ret, entry.getValue(), fieldNewValue);
-			}
-			return ret;
+			ret = clazz.newInstance();
 		} catch (InstantiationException | IllegalAccessException e) {
-			throw new BaseException(
-					"SYS_COMMON_MODEL2MODEL_8n3lk", 
-					ResMsgUtils.resolve("创建目标对象[{0}]时发生不可预料的异常，详情——{1}", ExceptionUtils.out(e)), 
-					e, 
-					ExceptionUtils.out(e));
+			throw new BaseException("beanUtils_8n3lk", "目标对象[{0}]实例化失败，详情——{1}", e, EU.out(e));
 		}
+		return bean2Bean(fieldMapping, obj, ret);
+	}
+
+	/**@param <S> 源数据的Javabean类型
+	 * @param <T> 目标数据类型
+	 * @param fieldMapping 源数据bean字段与目标bean字段的映射关系
+	 * @param obj 源数据bean
+	 * @param target 目标数据容器
+	 * @author hcqt@qq.com */
+	public static final <S, T> T bean2Bean(Map<String, String> fieldMapping, S obj, T target) {
+		if(null == fieldMapping) {
+			return target;
+		}
+		if(target == null) {
+			return null;
+		}
+		for (Entry<String, String> entry : fieldMapping.entrySet()) {
+			Object fieldNewValue = getFieldValue(obj, entry.getKey());
+			setFieldValue(target, entry.getValue(), fieldNewValue);
+		}
+		return target;
 	}
 
 	/** @author hcqt@qq.com */
-	private static final List<String> getAllFieldName (Class<?> clazz) {
-		List<String> ret = new ArrayList<String>();
-		if(null == clazz) {
+	public static final <T> T map2Bean(Class<T> beanClazz, Map<String, ?> map){
+		if(null == beanClazz || null == map){
+			return null;
+		}
+		T ret = null;
+		try {
+			ret = beanClazz.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new BaseException("beanUtils_9k3hQ", "map无法转换为javaBean，因为JavaBean无法实例化，详情——{0}", e, EU.out(e));
+		}
+		return map2Bean(ret, map);
+	}
+	
+	/** @author hcqt@qq.com */
+	public static final <T> T map2Bean(T beanObj, Map<String, ?> map){
+		if(null == beanObj || null == map){
+			return null;
+		}
+		Set<String> fields = getAllFieldName(beanObj.getClass());
+		if(fields == null) {
+			return null;
+		}
+		for (String field : fields) {
+			setFieldValue(beanObj, field, map.get(field));
+		}
+		return beanObj;
+	}
+
+	/** @author hcqt@qq.com */
+	@SuppressWarnings("unchecked")
+	public static final <T extends Collection<Map<String, ?>>, S> Collection<S> collectionMap2CollectionBean(Class<S> beanClazz, T collection) {
+		if(null == beanClazz || null == collection) {
+			return null;
+		}
+		Collection<S> ret = null;
+		try {
+			ret = collection.getClass().newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new BaseException("beanUtils_72jJk", "集合类型实例化失败，详情——{0}", e, e.getMessage());
+		}
+		for(Map<String, ?> item : collection){
+			S obj = map2Bean(beanClazz, item);
+			if(null == obj){
+				continue;
+			}
+			ret.add(obj);
+		}
+		if(!ret.isEmpty()){
 			return ret;
 		}
-		for (Class<? extends Object> currentClass = clazz; currentClass != Object.class; currentClass = currentClass.getSuperclass()) {
+		return null;
+	}
+
+	/**获取JavaBean的所有字段
+	 * @author hcqt@qq.com */
+	public static final Set<String> getAllFieldName (Class<?> clazz) {
+		if(null == clazz) {
+			return null;
+		}
+		Set<String> ret = new HashSet<>();
+		for (Class<?> currentClass = clazz; currentClass != Object.class; currentClass = currentClass.getSuperclass()) {
 			Field[] fields = currentClass.getDeclaredFields();
 			for (Field field : fields) {
 				ret.add(field.getName());
@@ -143,116 +264,56 @@ public class BeanUtils {
 		return ret;
 	}
 
-	/** @author hcqt@qq.com */
+	/**按照javaBean的get方法取值，若拿不到就直接从字段上取值
+	 * @author hcqt@qq.com */
 	public static final Object getFieldValue(
-			Object instanceObject, 
+			Object obj, 
 			String field) {
 		if(null == field) {
 			return null;
 		}
 		Method method = null;
-		for (Class<? extends Object> currentClass = instanceObject.getClass(); currentClass != Object.class; currentClass = currentClass.getSuperclass()) {
-			// 按照javaBean的get方法取值，若拿不到就直接从字段上取值
+		for (Class<?> currentClass = obj.getClass(); currentClass != Object.class; currentClass = currentClass.getSuperclass()) {
 			try {
 				method = currentClass.getDeclaredMethod(new StringBuilder().append("get")
 						.append(field.substring(0, 1).toUpperCase())
 						.append(field.substring(1)).toString());
-				return method.invoke(instanceObject);
+				return method.invoke(obj);
 			} catch (Throwable e) {
 				try {
 					Field _field = currentClass.getDeclaredField(field);
 					_field.setAccessible(true);
-					return _field.get(instanceObject);
-				} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e2) { }
+					return _field.get(obj);
+				} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e1) { }
 			}
 		}
 		return null;
 	}
 
-	/** @author hcqt@qq.com */
+	/**按照javaBean的set方法赋值，若拿不到就直接给字段上赋值
+	 * @author hcqt@qq.com */
 	public static final void setFieldValue(
-			Object instanceObject, 
+			Object obj, 
 			String field, 
-			Object fieldNewValue) {
+			Object val) {
 		if(null == field) {
 			return;
 		}
 		Method method = null;
-		for (Class<? extends Object> currentClass = instanceObject.getClass(); currentClass != Object.class; currentClass = currentClass.getSuperclass()) {
-			// 按照javaBean的set方法赋值，若拿不到就直接给字段上赋值
+		for (Class<?> currentClass = obj.getClass(); currentClass != Object.class; currentClass = currentClass.getSuperclass()) {
 			try {
 				method = currentClass.getDeclaredMethod(new StringBuilder().append("set")
 						.append(field.substring(0, 1).toUpperCase())
 						.append(field.substring(1)).toString());
-				method.invoke(instanceObject, fieldNewValue);
+				method.invoke(obj, val);
 			} catch (Throwable e) {
 				try {
 					Field _field = currentClass.getDeclaredField(field);
 					_field.setAccessible(true);
-					_field.set(instanceObject, fieldNewValue);
+					_field.set(obj, val);
 				} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e2) { }
 			}
 		}
-	}
-
-	/** @author hcqt@qq.com */
-	@SuppressWarnings("rawtypes")
-	public static final <T> T convertMap(Class<T> clazz, Map<String, Object> map){
-		if(null == clazz || null == map){
-			return null;
-		}
-		T t = null;
-		try {
-			t = clazz.newInstance();
-			Method[] methods= clazz.getMethods();
-			for(int i = 0; i < methods.length; i++){
-				Method method = methods[i];
-				String methodName = method.getName();
-				if(methodName.indexOf("set") == 0){
-					String key = methodName.substring(methodName.indexOf("set")+3);
-					StringBuilder _key = new StringBuilder(key.substring(0 , 1).toLowerCase()).append(key.substring(1));
-					
-					Object obj = map.get(_key.toString());
-					if(null != obj){
-						Class[] paramTypes = method.getParameterTypes();
-						if(obj.getClass().isAssignableFrom(paramTypes[0])){
-							try {
-								method.invoke(t, obj);
-							} catch (IllegalArgumentException e) {
-								e.printStackTrace();
-							} catch (InvocationTargetException e) {
-								e.printStackTrace();
-							}
-						}
-					}
-				}
-			}
-			return t;
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	/** @author hcqt@qq.com */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static final List<Object> convertListMap(Class clazz, List<Map<String, Object>> lists){
-		if(null == clazz || null == lists){
-			return null;
-		}
-		List<Object> _lists = new ArrayList<Object>();
-		for(Map<String, Object> item : lists){
-			Object obj = convertMap(clazz, item);
-			if(null != obj){
-				_lists.add(obj);
-			}
-		}
-		if(_lists.size() > 0){
-			return _lists;
-		}
-		return null;
 	}
 
 }
